331704314864ff1c7db14a05710db3853118a21b
dmanato <dmanato@iu.edu>
2025-11-05 14:45:16 -0800

    Refactor experiments and add real network script
    run_experiments_real_networks.py to automate DMoN + DICE experiments on real networks, including saving results and graph data for further analysis.

---
+++ run_experiments_real_networks.py
@@ -0,0 +1,176 @@
+'''
+This script runs experiments on real networks using the DMoN model and evaluates the effects of DICE attacks.
+It loads real network graphs, trains the DMoN model, applies DICE attacks, and computes metrics like ECS, M1, and M2.
+It saves the results in a CSV file for further analysis.
+
+Author: Dalya Manatova
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+from tqdm import tqdm
+import random
+import torch
+import argparse
+import os
+import itertools
+import csv
+import networkx as nx
+from lfr_generator import precompute_allpairs_neg_sqeuclidean, precompute_node_comm_neg_sqeuclidean
+import attacks
+from load_real_network import load_real_graph
+from torch_geometric.utils import from_networkx
+import time
+
+
+print("torch.cuda.is_available():", torch.cuda.is_available())
+print("Device count:", torch.cuda.device_count())
+print("Device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No CUDA")
+
+
+def train_model(data, true_labels, num_features=dim_features):
+    import dmon
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    num_clusters = len(set(true_labels))
+    model = dmon.DMoN(
+        in_channels=num_features, 
+        num_clusters=num_clusters, 
+        hidden_channels=128,
+        num_layers=2, # default is 1 for shallow model or 2 for typical GCN
+        dropout=0.5,  
+        gcn_skip=True,
+        collapse_regularization=1.0  
+    ).to(device)
+    # model = dmon.DMoN(in_channels=num_features, num_clusters=num_clusters, gcn_skip=True)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+    data = data.to(device)  # send features and edges to GPU
+    for epoch in range(201):
+        model.train()
+        optimizer.zero_grad()
+        ca, loss = model(data.x, data.edge_index)
+        loss.backward()
+        optimizer.step()
+        # if epoch % 20 == 0:
+        #     print(f"Epoch {epoch:03d} | Loss: {loss.item():.4f}")
+    model.eval()
+    data = data.to(device)  # ensure data is on same device for eval
+    ca, _ = model(data.x, data.edge_index)
+    pred_labels = ca.argmax(dim=1).cpu().numpy()  # bring prediction back to CPU
+    return pred_labels
+
+def main(args):
+    
+    results_dir = "real_graphs_dice"
+    os.makedirs(results_dir, exist_ok=True)
+
+    # sigma_c_values = args.sigma_c_values
+    # mu_values = args.mu_values
+    realizations = args.realizations
+    network_name = args.network_name
+    b_percentages = args.b_percentages
+    # min_community = args.min_community
+    seed = args.seed
+    results_rows = []
+
+    # random.seed(args.seed)
+    # np.random.seed(args.seed)
+    # torch.manual_seed(args.seed)
+
+    print(f"Loading real graph")
+    G, data, true_labels = load_real_graph(name=network_name)
+    dim_features = data.x.shape[1]
+    print(f"Number of nodes: {data.num_nodes}, Number of edges: {data.num_edges}, Feature dimension: {dim_features}")
+    # Precompute pairwise similarities for feature-based attacks
+    # _, S = precompute_allpairs_neg_sqeuclidean(G)
+    # _, S_nc = precompute_node_comm_neg_sqeuclidean(G, true_labels)
+    # === Save graph and membership ===
+    base_name = f"graph_{network_name}"
+    graph_file = os.path.join(results_dir, base_name + ".edgelist")
+    membership_file = os.path.join(results_dir, base_name + ".membership")
+    # Save the graph as an edge list
+    nx.write_edgelist(G, graph_file, delimiter=' ', data=False)
+    # Save the membership as a 1D array (each line: community ID)
+    np.savetxt(membership_file, true_labels, delimiter=' ', fmt='%d')
+    features_file = os.path.join(results_dir, base_name + ".features")
+    with open(features_file, "w", newline="") as f:
+        writer = csv.writer(f, delimiter=' ')
+        for node, node_data in G.nodes(data=True):
+            # Ensure feature is NumPy array of floats
+            x = node_data['x']
+            if isinstance(x, torch.Tensor):
+                x = x.detach().cpu().numpy()
+            row = [node] + list(map(float, x))
+            writer.writerow(row)
+
+    print(f"Generated and saved graph with {G.number_of_nodes()} nodes and {G.number_of_edges()} edges")
+
+    number_of_communities = len(set(true_labels))
+    print(f"Number of communities: {number_of_communities}")
+    for target_label in set(true_labels):
+        target_community = [nn for nn, label in enumerate(true_labels) if label == target_label]
+        target_size = len(target_community)
+        # Compute initial ECS, M1, M2 before attack r times for averaging
+        for realization in range(realizations):
+            pred_labels = train_model(data, true_labels, num_features=dim_features)
+            ecs_initial = attacks.compute_ECS(true_labels, pred_labels)
+            M1 = attacks.compute_M1(target_list=target_community, labels=pred_labels)
+            M2 = attacks.compute_M2(target_list=target_community, labels=pred_labels)
+        
+            results_rows.append([
+                        network_name, target_label, target_size, 0, 0, None, 0, ecs_initial, M1, M2, None
+                    ])
+        # Budget as % of intra-community edges
+        G_target = G.subgraph(target_community)
+        intra_edges = G_target.number_of_edges()
+        for p in b_percentages:
+            bb = int(np.round(p * intra_edges))
+            for p_val in args.p_values:
+                for realization in range(realizations):
+                    # print(f"Realization {realization+1}/{realizations} | mu={mu} sigma_c={sigma_c} label={target_label} b={bb}")
+                    start = time.time()
+                    G_attacked = attacks.dice_community_attack(G.copy(), target_community, bb, p=p_val)
+                    # G_attacked = attacks.dice_cfeature_comm_attack(G.copy(), target_community, true_labels, S_nc,  bb, p=p_val, feature_mode= args.attack_feature_mode)
+                    data_attacked = from_networkx(G_attacked)
+                    data_attacked.x = torch.stack([G_attacked.nodes[i]['x'] for i in range(len(G_attacked))])
+                    pred_labels_attacked = train_model(data_attacked, true_labels, num_features=dim_features)
+                    ecs = attacks.compute_ECS(true_labels, pred_labels_attacked)
+                    M1 = attacks.compute_M1(target_list=target_community, labels=pred_labels_attacked)
+                    M2 = attacks.compute_M2(target_list=target_community, labels=pred_labels_attacked)
+                    elapsed_time = time.time() - start
+                    results_rows.append([
+                        network_name, target_label, target_size, bb, p, p_val, realization+1, ecs, M1, M2, elapsed_time
+                    ])
+                print(f"Completed p = {p_val} and budget {p} for target label {target_label}")
+        # Save results to CSV
+    with open(args.outfile_csv, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["network_name", "target_label", "target_size", "b", 'b_percentage', "p", "realization", "ECS", "M1", "M2", "elapsed_time_sec"])
+        writer.writerows(results_rows)
+        print(f"Results saved to {args.outfile_csv}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run DMoN + DICE real network experiments")
+    parser.add_argument("--network_name", type=str, default="Wiki")
+    parser.add_argument("--realizations", type=int, default=50)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--outfile_csv", type=str, default=None)
+    parser.add_argument("--b_percentages", nargs="+", type=float, default=[0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85,  0.9, 0.95, 1])
+    # parser.add_argument("--b_percentages", nargs="+", type=float, default=[0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5])
+    parser.add_argument("--p_values", nargs="+", type=float, default=[0.0, 0.25, 0.5, 0.75, 1.0])
+    parser.add_argument("--attack_feature_mode", type=str, default=None, choices=[None, "connecting_node", "average_community"])
+
+    args = parser.parse_args()
+
+        # Dynamically set outfile_csv if not provided
+    if args.outfile_csv is None:
+        # Join mu values as a string for filename
+        mu_str = "_".join(str(mu) for mu in args.mu_values)
+        sigma_str = "_".join(str(sigma) for sigma in args.sigma_c_values)
+        p_str = "_".join(str(p) for p in args.p_values)
+        args.outfile_csv = f"dmon_dice_{mu_str}_sigma{sigma_str}_p_{p_str}_mincomm_{args.min_community}.csv"
+
+    print(f"Results will be saved to: {args.outfile_csv}")
+    main(args)
